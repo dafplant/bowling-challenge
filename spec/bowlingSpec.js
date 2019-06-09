cc5ec46e6003b2d34d176efad5d99512a0bf8fb3
Chris <chris.daf@gmail.com>
2019-06-09 19:31:14 +0100

    initial commit

---
+++ spec/bowlingSpec.js
@@ -0,0 +1,12 @@
+describe('Bowling', function(){
+
+    var game;
+
+    beforeEach(function (){
+        game = new Bowling();
+    });
+
+    it('can create a new game', function() {
+        var game = new Bowling();
+    });
+});
